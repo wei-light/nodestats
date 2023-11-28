1519078cdcd6cbff549e79707bd2c5e0f12ad191
tinylight <wei_light@qq.com>
2023-11-28 19:49:46 +0800

    feat(socket): add console info to client

--- packages/socket/client/index.ts
+++ packages/socket/client/index.ts
@@ -1,4 +1,5 @@
 import { type Socket, io } from 'socket.io-client'
+import logger from '@nodestats/logger'
 import type { ClientToServerEvents, ServerToClientEvents } from '../type'
 import { Status } from './status'
 
@@ -12,11 +13,15 @@ export function connectClientToServer(
 
   const status = new Status()
 
+  logger.info(`Try to connect to ${address} ...`)
+
   socket.on('connect', () => {
     socket.emit('client-server:ready', token)
   })
 
   socket.on('server-client:verified', async () => {
+    logger.success('Successfully connected to the server')
+
     while (socket.connected) {
       await status.update()
       socket.emit('client-server:push', token, status)
@@ -25,6 +30,16 @@ export function connectClientToServer(
     }
   })
 
-  socket.on('disconnect', () => {})
-  socket.on('connect_error', () => {})
+  socket.on('disconnect', (reason) => {
+    if (reason === 'io server disconnect')
+      logger.warn(`Disconnect: ${reason}, the token may be incorrect`)
+    else if (reason === 'transport close')
+      logger.warn(`Disconnect: ${reason}, the service may be down`)
+    else
+      logger.warn(`Disconnect: ${reason}`)
+  })
+
+  socket.on('connect_error', () => {
+    logger.error('Connect Error: check whether the connection options are correct')
+  })
 }
